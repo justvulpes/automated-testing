16f9decae4ecf0ccc77fe58716bee749de610c27
rareba <rrebase@gmail.com>
2017-09-26 10:55:43 +0300

    more tests

--- AT01/src/test/WeatherTest.java
+++ AT01/src/test/WeatherTest.java
@@ -3,51 +3,92 @@ package test;
 import org.junit.jupiter.api.Test;
 import weather.Weather;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Ragnar
  */
 public class WeatherTest {
 
+    /**Maximum and minimum possible temperatures.*/
+    public static final int MAX_TEMP = 50;
+    public static final int MIN_TEMP = -50;
+
     @Test
     void getCurrentTemperatureTooLow() {
         Weather weather = new Weather();
-        assertFalse(weather.getCurrentTemperatureTallinn() < -50);
+        assertFalse(weather.getCurrentTemperatureTallinn() < MIN_TEMP);
     }
 
     @Test
     void getCurrentTemperatureTooHigh() {
         Weather weather = new Weather();
-        assertFalse(weather.getCurrentTemperatureTallinn() > 50);
+        assertFalse(weather.getCurrentTemperatureTallinn() > MAX_TEMP);
     }
 
+    /**
+     * Check if the maximum temperature is higher or equal to minimum temperature.
+     */
     @Test
     void getMaxMinOfNext3DaysMaxHigherThanMin() {
+        Weather weather = new Weather();
+        for (Map map: weather.getMaxMinListOfNext3Days()) {
+            assertTrue((Integer)map.get("Max") >= (Integer) map.get("Min"));
+        }
     }
 
     @Test
     void getMaxMinOfNext3DaysProperAmountOfData() {
+        Weather weather = new Weather();
+        assertEquals(weather.getMaxMinListOfNext3Days().size(), 3);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(weather.getMaxMinListOfNext3Days().get(i).keySet().size(), 2);
+        }
     }
 
     @Test
     void getMaxMinOfNext3DaysTooHigh() {
+        Weather weather = new Weather();
+        for (Map map: weather.getMaxMinListOfNext3Days()) {
+            assertFalse((Integer)map.get("Max") >= MAX_TEMP);
+            assertFalse((Integer)map.get("Min") >= MAX_TEMP);
+        }
     }
 
     @Test
     void getMaxMinOfNext3DaysTooLow() {
+        Weather weather = new Weather();
+        for (Map map: weather.getMaxMinListOfNext3Days()) {
+            assertFalse((Integer)map.get("Max") <= MIN_TEMP);
+            assertFalse((Integer)map.get("Min") <= MIN_TEMP);
+        }
     }
 
     @Test
     void getMaxMinOfNext3DaysTooBigChange() {
+        Weather weather = new Weather();
+        int temp = weather.getMaxMinListOfNext3Days().get(0).get("Max");
+        for (Map map: weather.getMaxMinListOfNext3Days()) {
+
+            assertTrue(Math.abs((Integer)map.get("Max") - temp) < 30);
+            temp = (Integer)map.get("Max");
+        }
+        for (Map map: weather.getMaxMinListOfNext3Days()) {
+            assertTrue(Math.abs((Integer)map.get("Min") - temp) < 30);
+            temp = (Integer)map.get("Min");
+        }
     }
 
     @Test
     void getCoordsPossible() {
+        Weather weather = new Weather();
+        assertTrue(weather.getCoords().get("lat") > -90.0);
+        assertTrue(weather.getCoords().get("lat") < 90.0);
+        assertTrue(weather.getCoords().get("lon") < 180.0);
+        assertTrue(weather.getCoords().get("lon") > -180.0);
     }
-
-    @Test
-    void getCoordsFormat() {
-    }
-
 }
--- AT01/src/weather/Weather.java
+++ AT01/src/weather/Weather.java
@@ -1,5 +1,6 @@
 package weather;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,6 +9,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -17,7 +24,7 @@ public class Weather {
     private final static String BASE = "http://api.openweathermap.org/data/2.5/";
     private final static String API_POINT_WEATHER = "weather?id=";
     private final static String API_POINT_FORECAST = "forecast?id=";
-    private final static String CITY = "588409";  // Tallinn
+    private final static String CITY = "588409";  // id of Tallinn
     private final static String AND_APP_ID = "&APPID=";
     private final static String DEV_KEY = "f17ef112f87d82f2c607d0676b4245e8";
     private final static String AND_UNITS = "&units=metric";
@@ -25,8 +32,8 @@ public class Weather {
 
     public static void main(String[] args) {
         Weather weather = new Weather();
-//        weather.parseCurrentTemperatureInTallinn(weather.getData(BASE + API_POINT_WEATHER + CITY + AND_APP_ID + DEV_KEY + AND_UNITS));
-//        weather.parseForecast(weather.getData(BASE + API_POINT_FORECAST + CITY + AND_APP_ID + DEV_KEY + AND_UNITS));
+        weather.parseCurrentTemperatureInTallinn(weather.getData(BASE + API_POINT_WEATHER + CITY + AND_APP_ID + DEV_KEY + AND_UNITS));
+        weather.parseForecast(weather.getData(BASE + API_POINT_FORECAST + CITY + AND_APP_ID + DEV_KEY + AND_UNITS));
 
     }
 
@@ -60,18 +67,45 @@ public class Weather {
     }
 
     private void parseForecast(BufferedReader br) {
+        System.out.println("Current time: " + new Date());
         String output;
         try {
             while ((output = br.readLine()) != null) {
                 JSONObject jsonObject = new JSONObject(output);
-                System.out.println(jsonObject.getJSONArray("list"));
+                JSONArray jsonArray = jsonObject.getJSONArray("list");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String dateString = jsonArray.getJSONObject(i).getString("dt_txt");
+                    DateFormat df = new SimpleDateFormat("yyyy-mm-dd HH:MM:SS");
+                    Date startDate;  // this is glorious! :)
+                    startDate = df.parse(dateString);
+                    String newDateString = df.format(startDate);
+                    System.out.println(newDateString);
+//                    System.out.println(dateString);
+                }
             }
-        } catch (IOException | JSONException e) {
+        } catch (IOException | JSONException | ParseException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Get the
+     */
+    public List<Map<String, Integer>> getMaxMinListOfNext3Days() {
+        // example
+        Map<String, Integer> day1Data = Map.of("Max", 25, "Min", 15);
+        Map<String, Integer> day2Data = Map.of("Max", 27, "Min", 12);
+        Map<String, Integer> day3Data = Map.of("Max", 23, "Min", 8);
+        return List.of(day1Data, day2Data, day3Data);
+    }
+
     public int getCurrentTemperatureTallinn() {
-        return -999;
+        // example
+        return 0;
+    }
+
+    public Map<String, Double> getCoords() {
+        // example coords of Tallinn
+        return Map.of("lat", 59.437, "lon", 24.7535);
     }
 }
